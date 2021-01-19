f46325c6b9c2c51eee40a478137cec50e461beeb
Daniel Abel <dabel@nvidia.com>
2021-01-19 10:30:12 +0800

    hybrid embedding: first hybrid algorithm design commit

--- HugeCTR/include/embeddings/hybrid_sparse_embedding.hpp
+++ HugeCTR/include/embeddings/hybrid_sparse_embedding.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.
+ * Copyright (c) 2021, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,10 +20,29 @@
 
 namespace HugeCTR {
 
-template <typename TypeKey, typename TypeEmbedding>
+template <typename dtype, typename TypeEmbedding>
 class HybridSparseEmbedding : public IEmbedding {
- public:
-  HybridSparseEmbedding() {}
+private:
+
+  // the hybrid model
+  dtype num_frequent_;
+  dtype num_categories_;
+
+  std::vector<uint32_t> category_frequent_index_;
+  std::vector<dtype> frequent_categories_;
+
+  // gpu model instances
+  std::vector<FrequentEmbedding<dtype, TypeEmbedding>> frequent_embeddings_;
+  std::vector<InfrequentEmbedding<dtype, TypeEmbedding>> infrequent_embedding_;
+
+  // models_ and data_ are replications of the model and input data on each gpu
+  // the HybridSparseEmbedding class manages it's scope / frees the memory.
+  std::vector<HybridEmbeddingModel<dtype>> model_;
+  std::vector<HybridEmbeddingData<dtype>> data_;
+
+public:
+  HybridSparseEmbedding();
+  ~HybridSparseEmbedding();
 
   void forward(bool is_train) override;
   void backward() override;
@@ -38,5 +57,74 @@ class HybridSparseEmbedding : public IEmbedding {
   std::vector<TensorBag2> get_train_output_tensors() const override;
   std::vector<TensorBag2> get_evaluate_output_tensors() const override;
 };
+
+
+template <typename dtype>
+struct HybridEmbeddingModel {
+public:
+  HybridEmbeddingModel() {}
+  ~HybridEmbeddingModel() {}
+
+  dtype num_frequent;
+  dtype num_categories;
+
+  Tensor2<dtype> category_frequent_index;
+  Tensor2<uint32_t> category_location;
+};
+
+template <typename dtype>
+struct HybridEmbeddingData {
+  dtype num_tables;
+  std::vector<uint32_t> table_sizes;
+
+  // pointer to raw data: iteration from data reader
+  Tensor2<dtype> data;
+  // flattened data
+  Tensor2<dtype> samples;
+
+  // flatten raw input data
+  void calculate_samples();
+};
+
+
+// One FrequentEmbedding instance per gpu
+template <typename dtype, typename TypeEmbedding>
+class FrequentEmbedding {
+  // copy of the model parameters and the input data
+  HybridEmbeddingModel<dtype> model_;
+  HybridEmbeddingData<dtype> data_;
+
+  // locally stored embedding vectors for the data-parallel part of the embedding
+  Tensor2<TypeEmbedding> frequent_embedding_vectors_;
+  // locally stored reduced gradients: input for the all-reduce
+  Tensor2<TypeEmbedding> frequent_partial_gradients_;
+
+  void init();
+public:
+  FrequentEmbedding() {}
+  ~FrequentEmbedding() {}
+
+  void all_reduce();
+  void update();
+};
+
+
+template <typename dtype, typename TypeEmbedding>
+class InfrequentEmbedding {
+  Tensor2<TypeEmbedding> infrequent_embedding_vectors_;
+
+  // forward-send, backward-receive
+  Tensor2<dtype> model_indices_;
+  // forward-receive, backward-send
+  Tensor2<dtype> network_indices_;
+
+public:
+  InfrequentEmbedding();
+  ~InfrequentEmbedding();  
+
+  void all_to_all_forward();
+  void all_to_all_backward();
+};
+
 
 }  // namespace HugeCTR
